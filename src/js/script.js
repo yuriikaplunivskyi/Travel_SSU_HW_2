eb41ae11e3673137ab91e0c6726692f657317eb8
yuriikaplunivskyi <yurii.kaplunivskyi@gmail.com>
2023-01-13 15:13:34 +0200

    added adapt for display-size 480px

---
+++ src/js/script.js
@@ -0,0 +1,25 @@
+
+                
+window.addEventListener('DOMContentLoaded', () => {
+    const menu = document.querySelector('.menu'),
+    menuItem = document.querySelectorAll('.menu_item'),
+    hamburger = document.querySelector('.hamburger');
+
+    hamburger.addEventListener('click', () => {
+        hamburger.classList.toggle('hamburger_active');
+        menu.classList.toggle('menu_active');
+    });
+
+    menuItem.forEach(item => {
+        item.addEventListener('click', () => {
+            hamburger.classList.toggle('hamburger_active');
+            menu.classList.toggle('menu_active');
+        })
+    })
+})
+
+const slider = document.querySelector('.slider');
+const progress = document.querySelector('.progress');
+slider.oninput = function(){
+    progress.style.width = `${this.value}%`;
+};
